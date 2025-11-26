51860a4018ae4023dd750989b52230c77c70a031
BangJhen <jhenerar21@gmail.com>
2025-11-26 08:32:26 +0700

    first commit

---
+++ musikinAza/musikinAza.cpp
@@ -0,0 +1,8 @@
+//
+//  musikinAza.cpp
+//  musikinAza
+//
+//  Created by Ammar Ridho on 26/11/25.
+//
+
+#include <stdio.h>
---
+++ musikinAza/musikinAza.h
@@ -0,0 +1,12 @@
+//
+//  musikinAza.h
+//  musikinAza
+//
+//  Created by Ammar Ridho on 26/11/25.
+//
+
+#ifndef musikinAza_h
+#define musikinAza_h
+
+
+#endif /* musikinAza_h */
